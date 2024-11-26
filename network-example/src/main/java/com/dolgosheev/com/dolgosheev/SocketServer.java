aec5fb24cad3790c1ea3c25ded122682d1b80a4e
Aleksandr Dolgosheev <al.dolgosheev@gmail.com>
2024-11-26 23:03:22 +0300

    2024.11.26 update

---
+++ network-example/src/main/java/com/dolgosheev/com/dolgosheev/SocketServer.java
@@ -0,0 +1,25 @@
+package com.dolgosheev.com.dolgosheev;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.util.Scanner;
+
+public class SocketServer {
+    public static void main(String[] args) throws IOException {
+        try (var serverSocket = new ServerSocket(8081);
+             var socket = serverSocket.accept();
+             var inputStream = new DataInputStream(socket.getInputStream());
+             var outputStream = new DataOutputStream(socket.getOutputStream());
+             var scanner = new Scanner(System.in)) {
+
+            var request = inputStream.readUTF();
+            while (!request.equals("exit")) {
+                System.out.println("Клиент: " + request);
+                outputStream.writeUTF(scanner.nextLine());
+                request = inputStream.readUTF();
+            }
+        }
+    }
+}
